a925c0ce3faeef33428cf9f97ca6349cc01f1268
Mario <maming292@126.com>
2018-05-31 17:34:16 +0800

    18-05-31 下班

--- src/app/component/home/setting/station/station.component.ts
+++ src/app/component/home/setting/station/station.component.ts
@@ -7,11 +7,13 @@ import * as $ from "jquery";
 })
 export class StationComponent implements OnInit {
 	msglist: any;
+	alertshow: any;
 	page: number = 1;
 	maxPage: number = 1;
 	pageArr: any;
 	constructor() {}
 	ngOnInit() {
+		this.alertshow = false;
 		this.msglist = [
 			['广盈实业有限公司', '光伏电站', '办公园区', '440KW', '梁溪区', '7年', '10%', 120.34566, 30.12343],
 			['广盈实业有限公司', '光伏电站', '办公园区', '440KW', '梁溪区', '7年', '9%', 120.34566, 30.12343],
@@ -25,6 +27,14 @@ export class StationComponent implements OnInit {
 		for(let i = 1; i <= this.maxPage; i++) {
 			this.pageArr.push(i)
 		}
+	}
+	shows() {
+		this.alertshow = true;
+		console.log(this.alertshow)
+	}
+	hiddens() {
+		this.alertshow = false;
+		console.log(this.alertshow)
 	}
 
 	prev() {
