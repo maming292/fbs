836e745d8c8ca5a2068504a732dc369ad901ccd1
Mario <maming292@126.com>
2018-06-05 16:41:23 +0800

    ....

--- src/app/component/home/datadisplay/datadisplay.component.ts
+++ src/app/component/home/datadisplay/datadisplay.component.ts
@@ -34,59 +34,67 @@ export class DatadisplayComponent implements OnInit {
 		this.company_id = 141;
 		this.time = '2018-05';
 		this.xData = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27', '28', '29', '30', '31'];
-		this.num = [0, 0, 0, 0, 0, 52, 87, 52, 546, 5, 9, 4, 12, 457, 110, 211, 77, 44, 88, 99, 66, 33, 22, 55, 88, 33, 88, 77, 12, 98, 15]
-		//		this.getData(this.company_id,this.time);
-//		this.getData();
+//		this.num = [0, 0, 0, 0, 0, 52, 87, 52, 546, 5, 9, 4, 12, 457, 110, 211, 77, 44, 88, 99, 66, 33, 22, 55, 88, 33, 88, 77, 12, 98, 15]
+				this.getData(this.company_id,this.time);
+
 		this.setEchars(this.ectype, this.xData, this.num);
 
 		this.ecwidth = $('.ecList').width();
 		$('.ec').height(this.ecwidth * 0.50);
+		
+		
+		
+		
+		
+		
 	}
 
 	getData(company_id, time) {
-		console.log(`${this.service.path}/fbs/foreignForC/photovoltaic_chart?company_id=${company_id}&time=${time}`);
-		//		this.http.get(`${this.service.path}/fbs/foreignForC/photovoltaic_chart?company_id=${company_id}&time=${time}`)
-		//			.subscribe(
-		//				function(data) {
-		//					var num = [];
-		//					console.log(data.chart);
-		//					num[0] = data.chart.ONE;
-		//					num[1] = data.chart.TWO;
-		//					num[2] = data.chart.THREE;
-		//					num[3] = data.chart.FOUR;
-		//					num[4] = data.chart.FIVE;
-		//					num[5] = data.chart.SIX;
-		//					num[6] = data.chart.SEVEN;
-		//					num[7] = data.chart.EIGHT;
-		//					num[8] = data.chart.NINE;
-		//					num[9] = data.chart.TEN;
-		//					num[10] = data.chart.ELEVEN;
-		//					num[11] = data.chart.TWELVE;
-		//					num[12] = data.chart.THIRTEEN;
-		//					num[13] = data.chart.FOURTEEN;
-		//					num[14] = data.chart.FIVETEEN;
-		//					num[15] = data.chart.SIXTEEN;
-		//					num[16] = data.chart.SEVENTEEN;
-		//					num[17] = data.chart.EIGHTEEN;
-		//					num[18] = data.chart.NINETEEN;
-		//					num[19] = data.chart.TWENTY;
-		//					num[20] = data.chart.TWENTYONE;
-		//					num[21] = data.chart.TWENTYTWO;
-		//					num[22] = data.chart.TWENTYTHREE;
-		//					num[23] = data.chart.TWENTYFOUR;
-		//					num[24] = data.chart.TWENTYFIVE;
-		//					num[25] = data.chart.TWENTYSIX;
-		//					num[26] = data.chart.TWENTYSEVEN;
-		//					num[27] = data.chart.TWENTYEIGHT;
-		//					num[28] = data.chart.TWENTYNINE;
-		//					num[29] = data.chart.THIRTY;
-		//					num[30] = data.chart.THIRTYONE;
-		////					this.setEchars(this.ectype,this.xData,num);
-		//				}.bind(this),
-		//				function(err) {
-		//					console.log('失败');
-		//				}
-		//			);
+		console.log(company_id);
+		var num = [];
+//		console.log(`${this.service.path}/fbs/foreignForC/photovoltaic_chart?company_id=${company_id}&time=${time}`);
+				this.http.get(`${this.service.path}/fbs/foreignForC/photovoltaic_chart?company_id=${company_id}&time=${time}`)
+					.subscribe(
+						function(data) {
+							num[0] = data.chart.ONE;
+							num[1] = data.chart.TWO;
+							num[2] = data.chart.THREE;
+							num[3] = data.chart.FOUR;
+							num[4] = data.chart.FIVE;
+							num[5] = data.chart.SIX;
+							num[6] = data.chart.SEVEN;
+							num[7] = data.chart.EIGHT;
+							num[8] = data.chart.NINE;
+							num[9] = data.chart.TEN;
+							num[10] = data.chart.ELEVEN;
+							num[11] = data.chart.TWELVE;
+							num[12] = data.chart.THIRTEEN;
+							num[13] = data.chart.FOURTEEN;
+							num[14] = data.chart.FIVETEEN;
+							num[15] = data.chart.SIXTEEN;
+							num[16] = data.chart.SEVENTEEN;
+							num[17] = data.chart.EIGHTEEN;
+							num[18] = data.chart.NINETEEN;
+							num[19] = data.chart.TWENTY;
+							num[20] = data.chart.TWENTYONE;
+							num[21] = data.chart.TWENTYTWO;
+							num[22] = data.chart.TWENTYTHREE;
+							num[23] = data.chart.TWENTYFOUR;
+							num[24] = data.chart.TWENTYFIVE;
+							num[25] = data.chart.TWENTYSIX;
+							num[26] = data.chart.TWENTYSEVEN;
+							num[27] = data.chart.TWENTYEIGHT;
+							num[28] = data.chart.TWENTYNINE;
+							num[29] = data.chart.THIRTY;
+							num[30] = data.chart.THIRTYONE;
+							this.num = num;
+							this.setEchars(this.ectype, this.xData, this.num);
+					console.log(this.num);
+						}.bind(this),
+						function(err) {
+							console.log('失败');
+						}
+					);
 	}
 	setEchars(type,xData, num) {
 			$('#box').removeAttr('_echarts_instance_');
@@ -151,10 +159,15 @@ export class DatadisplayComponent implements OnInit {
 	
 	lineActive(){
 		this.btnChange = this.ecBtn[0];
-		this.setEchars('line', this.xData, this.num)
+		this.ectype  = 'line';
+		this.setEchars(this.ectype, this.xData, this.num)
 	}
 	barActive(){
 		this.btnChange = this.ecBtn[1];
-		this.setEchars('bar', this.xData, this.num)
+		this.ectype  = 'bar';
+		this.setEchars(this.ectype, this.xData, this.num)
+	}
+	a(e){
+		console.log(e)
 	}
 }
