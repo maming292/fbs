ffbe7cfc41442bf550525a5c89e3e85ab32008df
mario <maming292@163.com>
2018-07-20 14:15:59 +0800

    del

--- src/app/pipe/fixed-number.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FixedNumberPipe } from './fixed-number.pipe';
-
-describe('FixedNumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FixedNumberPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
--- src/typings.d.ts
+++
@@ -1,9 +0,0 @@
-/* SystemJS module definition */
-declare var module: NodeModule;
-declare var layer:any;
-declare var layui:any;
-declare var laydate:any;
-declare var laypage:any;
-interface NodeModule {
-  id: string;
-}
